20cda7dc0d057e80d34bda0e6535b6aadc762eea
haneul <108397012+haaaaane@users.noreply.github.com>
2022-10-21 18:11:55 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,22 +1,9 @@
-$('.swiper').slick({
-    dots:true,
-    autoplaySpeed:2500,
-    autoplay:true,
+$('.slide-group').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots:false,
     prevArrow:'<button class="arrleft"><i class="fas fa-angle-left"></i></button>',
-    nextArrow:'<button class="arrright"><i class="fas fa-angle-right"></i></button>',
-    slideToshow:5,
-    slideToScroll: 1,
-})
-
-var article5Near = $('.article5').offset().top - $(window).height()/2
-
-$(window).on('scroll', function() {
-    var sct = $(this).scrollTop()
-    if (sct > article5Near) {
-        $('.article5').addClass('on')
-    } else {
-        $('.article5').removeClass('on')
-    }
-    
-})
-
+    nextArrow:'<button class="arrright"><i class="fas fa-angle-right"></i></button>'
+});
